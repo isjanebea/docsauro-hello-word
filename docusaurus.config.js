a90b66a61a353ab5993b6e5e466d6b4ba30f8a80
Bea Ramerindo <beatriz.ramerindo@nuvemshop.com.br>
2022-08-10 12:45:15 -0300

    testE

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -17,7 +17,7 @@ const config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'isjanebea', // Usually your GitHub org/user name.
-  projectName: 'docsauro-hello-word.github.io', // Usually your repo name.
+  projectName: 'docsauro-hello-word', // Usually your repo name.
   trailingSlash: false,
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
